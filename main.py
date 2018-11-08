132dcb7d04c12ca0e5ebece494e0755783203254
Stanley Lee <stanleylee91776@gmail.com>
2018-11-08 14:26:21 -0800

    json dump

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import requests
 import json
 userdata = {"device": {"mobileType": "ios", "uid": ":“123E4567-E89B-12D3-A456-426655440000"}, "grantType": "device"}
-response = requests.post("https://web.spin.pm/api/v1/auth_tokens", json=userdata)
+data = requests.post("https://web.spin.pm/api/v1/auth_tokens", json=userdata)
 
-# print(response)
-print json.dumps(response, indent=4)
+jsonload = data.json()
+jsonload = json.dumps(jsonload)
+print(jsonload)
+# jsonload = jsonload.split("'")
