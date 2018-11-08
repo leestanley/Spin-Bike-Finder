f6cc0c7c1eb41af3dbf2c2f40dd88a9530a705a9
Stanley Lee <stanleylee91776@gmail.com>
2018-11-08 15:13:01 -0800

    beautified ebike finder

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 import requests
 import json
-userdata = {"device": {"mobileType": "ios", "uid": ":“123E5567-E89B-12D3-A456-426655440000"}, "grantType": "device"}
+import numpy as np
+from gtts import gTTS
+
+
+userdata = {"device": {"mobileType": "ios", "uid": ":“123E6567-E89B-12D3-A456-426655440000"}, "grantType": "device"}
 data = requests.post("https://web.spin.pm/api/v1/auth_tokens", json=userdata)
 
 jsonload = data.json()
 jsonload = json.dumps(jsonload)
-# print(jsonload)
 jsonload = jsonload.split('"')
-
 jwttoken = jsonload[3]
 
 jsonget = requests.get("https://web.spin.pm/api/v3/vehicles?lng=-117.2329987&lat=32.8777024&distance=&mode=", headers={'Authorization': 'Bearer ' + jwttoken})
 jsonloadget = jsonget.json()
-jsonloadget = json.dumps(jsonloadget)
 
-print(jsonloadget)
+#print(json.dumps(jsonloadget, indent=4, sort_keys=True))
+
+jsonarray = json.dumps(jsonloadget).split("{")
+ebikes = []
+for i in jsonarray:
+    if("ebike" in i):
+        ebikes.append(i)
+
+# print(jsonarray)
+
+
+print(ebikes, len(ebikes))
+# while True:
+#     for i in ebikes:
