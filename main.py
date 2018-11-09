bae7f0b6e7509490e572e9d23b942615e50f6bd3
Stanley Lee <stanleylee91776@gmail.com>
2018-11-09 02:13:57 -0800

    voices

--- main.py
+++ main.py
@@ -8,7 +8,6 @@ import time
 lng = -117.2329987
 lat = 32.8777024
 
-
 userdata = {"device": {"mobileType": "ios", "uid": ":“123E6568-E89B-12D3-A456-426655440000"}, "grantType": "device"}
 data = requests.post("https://web.spin.pm/api/v1/auth_tokens", json=userdata)
 
@@ -48,18 +47,28 @@ print(regbikes)
 #         splitray
 
 
-if(len(ebikes) > 1):
-    mytext = str(len(ebikes)) + 'E-Bikes Nearby and'
-    ortext = str(len(regbikes)) + "Bicycles Nearby"
-elif(len(ebikes) == 1):
-    mytext = str(len(ebikes)) + 'E-Bike Nearby'
+if(len(ebikes) == 1):
+    #mytext = str(len(ebikes)) + 'E-Bikes Nearby and'
+    #ortext = str(len(regbikes)) + "Bicycles Nearby"
+    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/1.m4a")
+elif(len(ebikes) == 2):
+    #mytext = str(len(ebikes)) + 'E-Bike Nearby'
+    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/2.m4a")
+elif(len(ebikes) == 3):
+    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/3.m4a")
+elif(len(ebikes) == 4):
+    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/4.m4a")
+elif(len(ebikes) == 5):
+    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/5.m4a")
 else:
-    mytext = 'No E-Bikes Nearby'
+    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/none.mp3")
 
-myobj = gTTS(text=mytext, slow=False)
-myobj.save("owo.mp3")
-os.system("owo.mp3")
-time.sleep(1.5)
-myobjs = gTTS(text=ortext, slow=False)
-myobjs.save("uwu.mp3")
-os.system("uwu.mp3")
+    #mytext = 'No E-Bikes Nearby'
+
+# myobj = gTTS(text=mytext, slow=False)
+# myobj.save("owo.mp3")
+# #os.system("owo.mp3")
+# time.sleep(2.5)
+# myobjs = gTTS(text=ortext, slow=False)
+# myobjs.save("uwu.mp3")
+# #os.system("uwu.mp3")
