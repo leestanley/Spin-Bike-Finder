4a7cd85726109e541a3e7a499d1088952232fa75
Stanley Lee <stanleylee91776@gmail.com>
2018-11-09 20:02:08 -0800

    update

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ lat = 32.8777024
 
 userdata = {"device": {"mobileType": "ios", "uid": ":“123E6568-E89B-12D3-A456-426655440000"}, "grantType": "device"}
 data = requests.post("https://web.spin.pm/api/v1/auth_tokens", json=userdata)
-
+print(data)
 jsonload = data.json()
 jsonload = json.dumps(jsonload)
 jsonload = jsonload.split('"')
@@ -28,10 +28,7 @@ for i in jsonarray:
     elif("bicycle" in i):
         regbikes.append(i)
 
-#print(ebikes, len(ebikes))
 splitray = ebikes[0].split(" ")
-print(regbikes)
-
 # x = splitray
 # x = map(str, x)
 # x = list(x)
@@ -46,25 +43,34 @@ print(regbikes)
 #     for i in ebikes:
 #         splitray
 
+print(len(ebikes))
 
-if(len(ebikes) == 1):
-    #mytext = str(len(ebikes)) + 'E-Bikes Nearby and'
-    #ortext = str(len(regbikes)) + "Bicycles Nearby"
-    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/1.m4a")
-elif(len(ebikes) == 2):
-    #mytext = str(len(ebikes)) + 'E-Bike Nearby'
-    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/2.m4a")
-elif(len(ebikes) == 3):
-    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/3.m4a")
-elif(len(ebikes) == 4):
-    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/4.m4a")
-elif(len(ebikes) == 5):
-    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/5.m4a")
-else:
-    os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/none.mp3")
 
-    #mytext = 'No E-Bikes Nearby'
+def voice():
+    if(len(ebikes) == 1):
+        #mytext = str(len(ebikes)) + 'E-Bikes Nearby and'
+        #ortext = str(len(regbikes)) + "Bicycles Nearby"
+        os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/1.m4a")
+    elif(len(ebikes) == 2):
+        #mytext = str(len(ebikes)) + 'E-Bike Nearby'
+        os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/2.m4a")
+    elif(len(ebikes) == 3):
+        os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/3.m4a")
+    elif(len(ebikes) == 4):
+        os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/4.m4a")
+    elif(len(ebikes) == 5):
+        os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/5.m4a")
+    elif(len(ebikes) > 5):
+        os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/nearby.m4a")
+    else:
+        os.system("C:/Users/Stanley/Desktop/Spin-Bike-Finder/haojin/none.mp3")
 
+
+voice()
+#mytext = 'No E-Bikes Nearby'
+# while True:
+#     voice()
+#     time.sleep(300)
 # myobj = gTTS(text=mytext, slow=False)
 # myobj.save("owo.mp3")
 # #os.system("owo.mp3")
